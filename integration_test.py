ce7d39dd6a7e8250d95ffa9f8b2d3849ac62cf6e
smascarenhas <samuel.fm@gmail.com>
2017-07-07 10:41:57 +0100

    added a folder scenarios with some example configurations that are being used to test the integration

--- integration_test.py
+++ integration_test.py
@@ -8,7 +8,47 @@ sys.path.append(fatima_lib_path)
 clr.AddReference("IntegratedAuthoringTool")
 
 from IntegratedAuthoringTool import IntegratedAuthoringToolAsset
+from IntegratedAuthoringTool import IATConsts
+from IntegratedAuthoringTool.DTOs import CharacterSourceDTO
+from RolePlayCharacter import RolePlayCharacterAsset
 
+# Load the Scenario Configuration
 iat = IntegratedAuthoringToolAsset.LoadFromFile("C:/Tests/test1.iat")
+print('- Scenario Information -')
+print('Name: ', iat.ScenarioName)
+print('Description: ', iat.ScenarioDescription)
+print('\n')
 
-print(iat.ScenarioName)
+
+# Load All The Character Sources
+sources = []
+for s in iat.GetAllCharacterSources():
+    sources.append(s.Source)
+
+# Loading the First Character From the Scenario
+rpc = RolePlayCharacterAsset.LoadFromFile(sources[0])
+rpc.LoadAssociatedAssets()
+iat.BindToRegistry(rpc.DynamicPropertiesRegistry)
+
+print('- Character Information -')
+print('Name: ', rpc.CharacterName)
+print('Mood: ', rpc.Mood)
+print('\n')
+
+curState = IATConsts.INITIAL_DIALOGUE_STATE
+while(curState != IATConsts.TERMINAL_DIALOGUE_STATE):
+    playerDialogs = iat.GetDialogueActionsByState(IATConsts.PLAYER, curState)
+    print('- Available Dialogue Options -')
+    dialogues = []
+    for d in playerDialogs:
+        dialogues.append(d)
+        print(d.Utterance)
+    i = -1
+    while(i < 0 or i >= len(dialogues)):
+        i = int(input('Select option: '))
+    pAct = iat.BuildSpeakActionName(IATConsts.PLAYER, dialogues[i].Id)
+    speakEvt = EventHelper.ActionEnd(IATConsts.PLAYER, pAct, rpc.CharacterName)
+
+
+# var actionName = iat.BuildSpeakActionName(playerStr, chosenDialog.Id)
+# var speakEvt = EventHelper.ActionEnd(playerStr, actionName.ToString(), rpc.CharacterName.ToString())
